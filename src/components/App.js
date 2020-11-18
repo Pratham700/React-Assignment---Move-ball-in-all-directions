d165e246cf38006c905fc396649ea2c1e18b36b8
Pratham700 <prathamsinghal.singhal@gmail.com>
2020-11-18 13:34:33 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,72 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+
+
+  const reset = () => {
+    setRenderBall(false) ;
+    setX(0) ;
+    setY(0) ;
+    setBallPosition({
+      left: "0px",
+      top: "0px",
+    });
+  };
+
+  
+   const start  = () => {
+        setRenderBall(true) ;  
+   }
+
+
+  const renderChoice = () => {
+    return renderBall ? (
+      <div className="ball" style = {{
+        position:"absolute",
+        left : ballPosition.left,
+        top :  ballPosition.top,
+      }}></div>
+    ):(
+      <button onClick={start} className = "start">
+        Start
+      </button>
+    )
+  };
+
+
+
+  const updateXY = (newX,newY) => {
+    setX(newX) ;
+    setY(newY) ;
+    setBallPosition({
+      left: newX + "px" ,
+      top: newY+ "px" 
+     });
+  }
+
+  useEffect( () => {
+    const keyListener = (evt) => {
+      if(renderBall) {
+        if(evt.keycode === 37) {
+          updateXY(x-5,y);
+        }
+        else if(evt.keyCode === 38) {
+          updateXY(x,y-5) ;
+        }else if(evt.keyCode === 39) {
+            updateXY(x+5,y) ;
+        }else if(evt.keCode === 40){
+          updateXY(x,y+5) ;
+        }
+      }
+    };
+    
+    document.addEventListener('keydown',keyListener) ;
+
+    return () => document.removeEventListener('keydown',keyListener) ;
+  }) ;
+
+
 
   return (
     <div className="playground">
